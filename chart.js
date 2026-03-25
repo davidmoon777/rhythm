c839c242500c1c03431689bd4aeb834065967835
davidmoon777 <davidmoon0731@gmail.com>
2026-03-25 16:32:16 +0900

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,10 @@
+const chart = [
+{time:1000,lane:0,type:"normal"},
+{time:1500,lane:1,type:"normal"},
+{time:2000,lane:2,type:"long",duration:800},
+{time:2500,lane:3,type:"normal"},
+{time:3000,lane:0,type:"normal"},
+{time:3200,lane:1,type:"normal"},
+{time:3400,lane:2,type:"normal"},
+{time:3600,lane:3,type:"normal"},
+]
